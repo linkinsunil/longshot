34739fbd6e350e220d3aa38fe845a796eb0e73f8
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-12-02 17:25:48 +0530

    Feat: Added routes for the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Drag from './components/Drag/Drag';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='container'>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Dashboard />} />
+          <Route path='/outlines' element={<Drag />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
